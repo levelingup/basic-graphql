be3e199392fbfa50ef422026a3ba4f7cf25c677f
Andrew <ahliu1@gmail.com>
2020-02-12 07:47:23 -0800

    delete user mutation

--- schema/schema.js
+++ schema/schema.js
@@ -79,6 +79,17 @@ const mutation = new GraphQLObjectType({
       resolve(parentValue, { firstName, age }) {
         return axios.post("http://localhost:3000/users", { firstName, age });
       }
+    },
+    deleteUser: {
+      type: UserType,
+      args: {
+        id: { type: new GraphQLNonNull(GraphQLString) }
+      },
+      resolve(parentValue, { id }) {
+        return axios
+          .delete(`http://localhost:3000/user/${id}`)
+          .then(res => res.data);
+      }
     }
   }
 });
